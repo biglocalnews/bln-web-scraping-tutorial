9c716c53baba502ded6be326d9f7d244233f9dd7
Eric Sagara <esagara@gmail.com>
2023-11-22 15:36:27 -0700

    added gitignore, more work on fdic scraper

--- scripts/fdic.py
+++ scripts/fdic.py
@@ -13,3 +13,38 @@ response.raise_for_status()
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
+results = []
+
+table = soup.find('table')
+
+table_head = table.find('thead')
+spans = table_head.find_all('span', {'class': 'dtfullname'})
+
+fieldnames = []
+
+for span in spans:
+    fieldnames.append(span.text)
+
+fieldnames.append('url')
+
+results.append(fieldnames)
+
+table_body = table.find('tbody')
+
+rows = table_body.find_all('tr')
+
+for row in rows:
+    cells = row.find_all('td')
+    values = []
+    for cell in cells:
+        values.append(cell.text)
+
+    bank_link = row.find('a')
+    href = bank_link['href']
+    bank_url = 'https://www.fdic.gov/' + href
+    values.append(bank_url)
+
+    results.append(values)
+
+
+# TODO: write out results to csv file
